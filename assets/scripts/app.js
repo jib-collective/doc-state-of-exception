33b43ddfe1ca0e6b6284dc701fb22acb5fccada7
Gustav Pursche <pursche@posteo.de>
2017-07-08 14:46:08 +0100

    Improve styles for supersmall screens

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,13 @@ import { init as slider } from './slider';
 import { init as fullscreen } from './fullscreen';
 
 domready(() => {
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
   slider();
   fullscreen();
+
+  // hide iOS address bar
+  if (isIOS) {
+    setTimeout(() => window.scrollTo(0, 1), 0);
+  }
 });
